c73240c135a5c51a2c0ec860aa5403d8b8fda783
DhanushNatarajan05 <dhanush.snatarajan@gmail.com>
2026-01-13 12:47:23 +0530

    list prg1

---
+++ list prg1.py
@@ -0,0 +1,6 @@
+num_list=[]
+n=int(input("Enter the number of elements:"))
+for i in range(n):
+    num=int(input("Enter an integer:"))
+    num_list.append(num)
+    print("List of integers:",num_list)
